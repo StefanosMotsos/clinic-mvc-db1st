eea3832be50b9d752bf6e326a6265956245b594b
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-06-19 23:56:35 +0300

    feat: added more filters and EntityForbiddenException

---
+++ ClinicApp/Core/Filters/DoctorFiltersDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ClinicApp.Core.Filters
+{
+    public class DoctorFiltersDTO
+    {
+        public string? Username { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? Email { get; set; }
+        public string? Specialty { get; set; }
+    }
+}
---
+++ ClinicApp/Core/Filters/PatientFiltersDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinicApp.Core.Filters
+{
+    public class PatientFiltersDTO
+    {
+        public string? Username { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? Email { get; set; }
+        public string? Amka { get; set; }
+        public string? BloodType { get; set; }
+    }
+}
